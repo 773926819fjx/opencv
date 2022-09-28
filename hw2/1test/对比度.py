7b861bac3b8a6dfb41bad8b2c45563c77322e1b4
maniact <773926819@qq.com>
2022-09-28 18:29:17 +0800

    maniact

---
+++ hw2/1test/对比度.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+imgfile = "C:\\Users\\Administrator\\Desktop\\1\\1\\6.png"
+
+img = cv2.imread(imgfile)
+b, g, r = cv2.split(img)
+bH = cv2.equalizeHist(b)
+gH = cv2.equalizeHist(g)
+rH = cv2.equalizeHist(r)
+result = cv2.merge((bH, gH, rH))
+cv2.HoughLinesP
+cv2.namedWindow("Input", 0)
+cv2.resizeWindow("Input", 640, 480)
+cv2.imshow("Input", img)
+cv2.namedWindow("Result", 0)
+cv2.resizeWindow("Result", 640, 480)
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# plt.imshow(imgRGB)
+# plt.show()
