f9a5c91d1ee0d0baabe6edeb5335503526ad2802
maniact <773926819@qq.com>
2022-09-30 14:57:50 +0800

    maniact

--- hw2/3test/去阴影.py
+++ hw2/3test/去阴影.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
+import skimage
 import matplotlib
 from matplotlib import pyplot as plt
 
-img = cv2.imread('hw2\\3\\25.jpg', 0)
-f = np.fft.fft2(img)
-fshift = np.fft.fftshift(f)
-fimg = np.log(np.abs(fshift))
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']
-plt.subplot(121), plt.imshow(img, 'gray'), plt.title(u'(a)原始图像')
-plt.axis('off')
-plt.subplot(122), plt.imshow(fimg, 'gray'), plt.title(u'(b)傅里叶变换处理')
-plt.axis('off')
-plt.show()
+img_original = cv2.imread('hw2\\3\\28.jpg')
+img_original_standard = img_original / 255
+#添加高斯噪声，函数返回的归一化的多维数组
+img_noise = skimage.util.random_noise(img_original_standard, mode='gaussian')
+#高斯滤波
+img_blur = cv2.GaussianBlur(img_noise, (127, 127), 15)
+imgs = img_blur
+img_original=img_original.astype(np.float32)
+img_blur=img_blur.astype(np.float32)
+img = cv2.divide(img_blur,img_original )
+cv2.imshow('Images', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
